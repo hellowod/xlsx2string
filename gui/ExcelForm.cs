2b00659e892bc64665227e085c2f3f275b564de9
abaojin <abaojin@gmail.com>
2017-04-04 23:54:51 +0800

    gui
    Signed-off-by: abaojin <abaojin@gmail.com>

--- gui/ExcelForm.cs
+++ gui/ExcelForm.cs
@@ -15,17 +15,9 @@ namespace xlsx2string
     {
         public ExcelForm()
         {
-            //AppDomain.CurrentDomain.AssemblyResolve += (sender, args) => {
-            //    string resourceName = new AssemblyName(args.Name).Name + ".dll";
-            //    string resource = Array.Find(this.GetType().Assembly.GetManifestResourceNames(), element => element.EndsWith(resourceName));
-
-            //    using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resource)) {
-            //        Byte[] assemblyData = new Byte[stream.Length];
-            //        stream.Read(assemblyData, 0, assemblyData.Length);
-            //        return Assembly.Load(assemblyData);
-            //    }
-            //};
-            InitializeComponent();
+            this.MaximizeBox = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.InitializeComponent();
         }
 
         private void label1_Click(object sender, EventArgs e)
