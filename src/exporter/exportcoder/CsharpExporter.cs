438ffa3220a1f8d3146452120a1e3c0cb9d7ecd9
abaojin <abaojin@gmail.com>
2017-04-05 19:41:46 +0800

    exporter
    Signed-off-by: abaojin <abaojin@gmail.com>

--- src/exporter/exportcoder/CsharpExporter.cs
+++ src/exporter/exportcoder/CsharpExporter.cs
@@ -36,37 +36,7 @@ namespace xlsx2string
 
         public override void SaveToFile(string filePath, Encoding encoding)
         {
-            if (fieldList == null) {
-                throw new Exception("CSDefineGenerator内部数据为空。");
-            }
-
-            string defName = Path.GetFileNameWithoutExtension(filePath);
-
-            // 创建代码字符串
-            StringBuilder sb = new StringBuilder();
-            sb.AppendLine("//");
-            sb.AppendLine("// Auto Generated Code By excel2json");
-            sb.AppendLine("//");
-            sb.AppendLine();
-            if (this.ClassComment != null)
-                sb.AppendLine(this.ClassComment);
-            sb.AppendFormat("public class {0}\r\n{{", defName);
-            sb.AppendLine();
-
-            foreach (FieldDef field in fieldList) {
-                sb.AppendFormat("\tpublic {0} {1}; // {2}", field.type, field.name, field.comment);
-                sb.AppendLine();
-            }
-
-            sb.Append('}');
-            sb.AppendLine();
-            sb.AppendLine("// End of Auto Generated Code");
-
-            // 保存文件
-            using (FileStream file = new FileStream(filePath, FileMode.Create, FileAccess.Write)) {
-                using (TextWriter writer = new StreamWriter(file, encoding))
-                    writer.Write(sb.ToString());
-            }
+            
         }
 
         public override void Init()
